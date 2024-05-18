ea6b0970d1f58759ab13d66397e40ff027fa7286
niccolò cherubin <niccolo.cherubin@iisviolamarchesini.edu.it>
2024-05-18 10:23:44 +0200

    fatto home page con slideshow
    Da mettere immagini full screeen o centrate

--- Progetto/scripts/home.js
+++ Progetto/scripts/home.js
@@ -1,19 +1,56 @@
-let currentStop = 1;
+let currentRestaurant = 0;
 
-document.getElementById('next').addEventListener('click', () => {
-    currentStop++;
-    if (currentStop > 4) {
-        currentStop = 1;
-    }
-    showStop(currentStop);
-});
+const creazione = function(){
+    let div = document.body.querySelector(".image-container");
 
-function showStop(stopNumber) {
+    let section = document.createElement("section");
+    section.id = `ristorante-${currentRestaurant}`;
+
+    let titolo = document.createElement("h2");
+    titolo.textContent = ristoranti[currentRestaurant].name;
+
+    let immagine = document.createElement("img");
+    immagine.src = ristoranti[currentRestaurant].image;
+
+
+    let specializzazione = document.createElement("p");
+    specializzazione.textContent = `Spcecializzazione:${ristoranti[currentRestaurant].specialization}`;
+
+    let stasrs = document.createElement("p");
+    stasrs.textContent = `Stelle:${ristoranti[currentRestaurant].stars}`;
+
+    let informazioni = document.createElement("p");
+    informazioni.textContent = `Informazioni:${ristoranti[currentRestaurant].informations}`;
+
+    section.appendChild(titolo);
+    section.appendChild(immagine);
+    section.appendChild(specializzazione);
+    section.appendChild(stasrs);
+    section.appendChild(informazioni);
+
+    div.appendChild(section);
+}
+
+function showRestaurant(stopNumber) {
     const stops = document.querySelectorAll('section');
     stops.forEach((stop) => {
         stop.style.display = 'none';
     });
-    document.getElementById(`stop-${stopNumber}`).style.display = 'block';
+    let ristorante = document.querySelector(`#ristorante-${stopNumber}`);
+    console.log(`ristorante-${stopNumber}`)
+    ristorante.style.display = "block";
 }
 
-showStop(currentStop);
+document.body.querySelector("#next").addEventListener('click', () => {
+    currentRestaurant++;
+    if (currentRestaurant > ristoranti.length) 
+    {
+        currentRestaurant = 1;
+    }
+    creazione();
+    
+    showRestaurant(currentRestaurant);
+});
+
+
+showRestaurant(currentRestaurant);
