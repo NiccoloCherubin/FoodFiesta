47ed7c9c5947b3e692fda903f54b330ccbf3b92e
Grappeggia Niccolò <niccolo.grappeggia@iisviolamarchesini.edu.it>
2024-05-18 08:19:15 +0200

    Finita Lista Ristoranti

--- Progetto/scripts/listaRistoranti.js
+++ Progetto/scripts/listaRistoranti.js
@@ -2,30 +2,38 @@
 function displayRestaurants(restaurants) {
   const container = document.getElementById("restaurants-container");
 
-  restaurants.forEach((restaurant) => {
+  restaurants.forEach((restaurant, index) => {
     const restaurantElement = document.createElement("div");
     restaurantElement.classList.add("restaurant");
 
     restaurantElement.innerHTML = `
         <div class="container">
             <div class="card">
-            <h1>${restaurant.name}</h1>
-            <img src="${restaurant.image}" alt="${restaurant.name}">
-            <p>Specializzazione: ${restaurant.specialization}</p>
-            <p>Stelle: ${restaurant.stars}</p>
-            <ul>
-                ${restaurant.informations
-                  .map(
-                    (info) =>
-                      `<li><strong>${info.name}:</strong> ${info.value}</li>`
-                  )
-                  .join("")}
-            </ul>
+                <h1>${restaurant.name}</h1>
+                <img src="${restaurant.image}" alt="${restaurant.name}">
+                <p>Specializzazione: ${restaurant.specialization}</p>
+                <p>Stelle: ${restaurant.stars}</p>
+                <ul>
+                    ${restaurant.informations
+                      .map(
+                        (info) =>
+                          `<li><strong>${info.name}:</strong> ${info.value}</li>`
+                      )
+                      .join("")}
+                </ul>
+                <input type="submit" value="Prenota" class="bookButton">
             </div>
         </div>
         `;
 
     container.appendChild(restaurantElement);
+
+    // Gestore di eventi click per i bottoni di prenotazione
+    const button = restaurantElement.querySelector('.bookButton');
+    button.addEventListener('click', () => {
+      localStorage.setItem('selectedRestaurantIndex', index);
+      alert(`Ristorante selezionato: ${restaurant.name}`);
+    });
   });
 }
 displayRestaurants(ristoranti);
