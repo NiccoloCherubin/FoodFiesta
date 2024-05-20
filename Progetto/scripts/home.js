4d5aa2c23023af2f4859527e8cb228378fbc55b6
Niccolò Cherubin <niccolo.cherubin@ghostprison.onmicrosoft.com>
2024-05-20 17:29:24 +0200

    finito homepage

--- Progetto/scripts/home.js
+++ Progetto/scripts/home.js
@@ -2,22 +2,22 @@ let currentRestaurant = 0;
 
 //creazione carasoul di ristoranti
 const creazione = function(){
-    let div = document.body.querySelector(".contenitore-immagini");
+    var div = document.body.querySelector(".contenitore-immagini");
 
     //sezione che conterrà immagini e titolo
-    let section = document.createElement("section");
+    var section = document.createElement("section");
     section.id = `ristorante-${currentRestaurant}`;
 
     //titolo
-    let titolo = document.createElement("h2");
+    var titolo = document.createElement("h2");
     titolo.textContent = ristoranti[currentRestaurant].name;
 
     //immagine
-    let immagine = document.createElement("img");
+    var immagine = document.createElement("img");
     immagine.src = ristoranti[currentRestaurant].image;
 
     //link che porta alla lista dei ristoranti
-    let a = document.createElement("a");
+    var a = document.createElement("a");
     a.href ="listaRistoranti.html";
 
     a.append(immagine);
@@ -50,14 +50,20 @@ document.body.querySelector("#next").addEventListener('click', () => {
 });
 
 const ristoranteMese = function(){
-    let divRistoranteMese = document.body.querySelector("ristorante-mese");
+    var divRistoranteMese = document.body.querySelector("#ristorante-mese");
+
+    //calcolo dell'indice casuale con il random
+    let indiceCasuale = Math.floor(Math.random() * ristoranti.length);
+
+    var titolo = document.createElement("h2");
+    titolo.textContent = ristoranti[indiceCasuale].name;
     
-    
-    let imgRistoranteMese = document.createElement("img");
+    var imgRistoranteMese = document.createElement("img");
     
     //calcolo in modo randomatico il ristorante del mese
-    imgRistoranteMese.src = ristoranti[1].image;
+    imgRistoranteMese.src = ristoranti[indiceCasuale].image;
     
+    divRistoranteMese.appendChild(titolo);
     divRistoranteMese.appendChild(imgRistoranteMese);
     
     showRestaurant(currentRestaurant);
