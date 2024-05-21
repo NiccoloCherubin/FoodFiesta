0ddc76f70dece4b20628eb43907e2f6ad6db0edd
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2024-05-21 08:38:12 +0200

    Immagini ristoranti in locale

--- Progetto/scripts/datiRistoranti.js
+++ Progetto/scripts/datiRistoranti.js
@@ -4,7 +4,7 @@ const ristoranti = [
         "name": "Pissa e pissa",
         "specialization": "Ristorante e pizzeria",
         "stars": 4.5,
-        "image": "https://lh3.googleusercontent.com/p/AF1QipOgRT3q3I2TC-75hProq6sCiVEPdG6LIPEcf0ua=s680-w680-h510",
+        "image": "assets/ristoranti/pissa_e_pissa.png",
         "informations": [
             { "name": "Indirizzo", "value": "Via Roma, 1 - 00100 Roma" },
             { "name": "Numero di telefono", "value": "+39 123 123 1234" },
@@ -17,7 +17,7 @@ const ristoranti = [
         "name": "La Trattoria",
         "specialization": "Ristorante italiano",
         "stars": 4.0,
-        "image": "https://www.trattoriadadanilo.com/wp-content/uploads/trattoria-da-danilo-mhp05.jpg",
+        "image": "assets/ristoranti/la_trattoria.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Via Milano, 10 - 20100 Milano" },
             { "name": "Numero di telefono", "value": "+39 234 234 2345" },
@@ -30,7 +30,7 @@ const ristoranti = [
         "name": "Sushi House",
         "specialization": "Ristorante giapponese",
         "stars": 4.7,
-        "image": "https://www.aprireazienda.com/wp-content/uploads/2019/11/aprire-un-ristorante-giapponese-1024x683.jpg",
+        "image": "assets/ristoranti/sushi_house.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Via Torino, 15 - 10100 Torino" },
             { "name": "Numero di telefono", "value": "+39 345 345 3456" },
@@ -43,7 +43,7 @@ const ristoranti = [
         "name": "Burger Deluxe",
         "specialization": "Hamburgeria",
         "stars": 4.2,
-        "image": "https://www.industrypancalieri.com/wp-content/uploads/2023/10/hamburgeria-torino.jpg",
+        "image": "assets/ristoranti/burger_deluxe.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Via Napoli, 20 - 50100 Firenze" },
             { "name": "Numero di telefono", "value": "+39 456 456 4567" },
@@ -56,7 +56,7 @@ const ristoranti = [
         "name": "Gelateria Gusto",
         "specialization": "Gelateria",
         "stars": 4.8,
-        "image": "https://www.gelaterialatosca.it/wp-content/uploads/2023/10/gusti2-gelateria-la-tosca-torino.jpg",
+        "image": "assets/ristoranti/gelateria_gusto.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Piazza della Repubblica, 5 - 40100 Bologna" },
             { "name": "Numero di telefono", "value": "+39 567 567 5678" },
@@ -69,7 +69,7 @@ const ristoranti = [
         "name": "Osteria del Mare",
         "specialization": "Ristorante di pesce",
         "stars": 4.6,
-        "image": "https://www.bcoolguide.com/wp-content/uploads/coste/molo_22.jpg",
+        "image": "assets/ristoranti/osteria_del_mare.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Via del Porto, 10 - 60100 Ancona" },
             { "name": "Numero di telefono", "value": "+39 678 678 6789" },
@@ -82,7 +82,7 @@ const ristoranti = [
         "name": "La Cantina",
         "specialization": "Enoteca e ristorante",
         "stars": 4.3,
-        "image": "https://le-strade.com/wp-content/uploads/2022/11/20221123-La-Botte-002-min.jpg",
+        "image": "assets/ristoranti/la_cantina.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Via delle Vigne, 20 - 80100 Napoli" },
             { "name": "Numero di telefono", "value": "+39 789 789 7890" },
@@ -95,7 +95,7 @@ const ristoranti = [
         "name": "Trattoria Toscana",
         "specialization": "Cucina toscana",
         "stars": 4.5,
-        "image": "https://static.gamberorosso.it/2020/06/toscana-casale.jpg",
+        "image": "assets/ristoranti/trattoria_toscana.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Via Firenze, 25 - 50100 Firenze" },
             { "name": "Numero di telefono", "value": "+39 890 890 8901" },
@@ -108,7 +108,7 @@ const ristoranti = [
         "name": "Bistro Francese",
         "specialization": "Cucina francese",
         "stars": 4.7,
-        "image": "https://static.gamberorosso.it/media/k2/items/src/6312f58c7a7cbaa51484f9ea085b4e32.jpg",
+        "image": "assets/ristoranti/bistro_francese.jpg",
         "informations": [
             { "name": "Indirizzo", "value": "Piazza Parigi, 3 - 00100 Roma" },
             { "name": "Numero di telefono", "value": "+39 901 901 9012" },
